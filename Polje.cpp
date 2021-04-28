99ca0d2a302672bd3eb271dff1e587e953ca55d7
Sara Misic <saram140100@gmail.com>
2021-04-28 10:15:08 +0200

    commit1
    klasa polja,podmornice i podmornica sa nizom

--- Polje.cpp
+++ Polje.cpp
@@ -1,31 +1,97 @@
-#include "polje.h"
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
 
-namespace polja{
-    Polje::Polje(){   
-    }
+    class Polje
+    {
 
-    static bool Polje::provera(int x, int y){
-        if(x<0 || y<0)
-            return false;
-        else
-            return true;
-    }
-    void Polje::postavi(int x, int y){
-        if(Polje::provera(x, y)){
+            int _x;
+            int _y;
+        public:
+        Polje(int x,int y)
+        {
             _x=x;
             _y=y;
-        }else{
-            _x=0;
-            _y=0;
         }
+
+        int getX()
+        {
+            return _x;
+        }
+        
+        int getY()
+        {
+            return _y;
+        }
+
+
+        void ispisPolja()
+        {
+            std::cout<<"("<<_x<<", "<<_y<<")"<<std::endl;
+        }
+
+    };
+
+
+
+    class Podmornica
+    {   
+        
+    public:
+        int duzina;
+        std::vector<Polje> podmornica;
+        Podmornica(std::vector<Polje>);
+        int proveraPogotka(Polje polje);
+
+    };    
+
+    Podmornica::Podmornica(std::vector<Polje> polja)
+    {
+            podmornica=polja;
+            duzina=podmornica.size();
     }
-    Polje::Polje(int x, int y){
-        postavi(x,y);
+    
+
+    int Podmornica::proveraPogotka(Polje p1)
+    {
+        if(std::binary_search(Podmornica::podmornica.begin(),Podmornica::podmornica.end(),p1))
+            return 1;
+        else 
+            return 0;
+
+        // vraca 1 ako je u podmornici polje koje je protivnik gadjao, nula ako nije
     }
-    int Polje::vratiX() const{
-        return _x;
-    }
-    int Polje::vratiY() const{
-        return _y;
-    }
+
+
+    class PodmornicaSaNizom{
+            public:
+                std::pair<Podmornica,std::vector<int>> podmornicaSaNizom;
+                PodmornicaSaNizom(Podmornica);
+                void oznaciPogodjenoPolje(Polje polje);
+                void potopiPodmornicu(Podmornica podmornica);
+
+    
+        PodmornicaSaNizom(Podmornica podmornica)
+        {
+            std::vector<int> pomocni(podmornica.duzina,0);
+            podmornicaSaNizom=std::make_pair(podmornica,pomocni);
+        }
+
+        void oznaciPogodjenoPolje(Polje polje)
+        {
+
+        }
+    
+    };
+
+
+
+int main()
+{
+    Polje polje(2,3);
+    polje.ispisPolja();
+
+    return 0;
+
 }
